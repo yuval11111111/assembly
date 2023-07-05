1601b0708862e8759043515c51554c5996d359e9
yuval11111111 <87775280+yuval11111111@users.noreply.github.com>
2023-07-05 10:53:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const fs = require('fs');
+var line = 1
+fs.readFile("./program.txt", "utf8", (err, P) => {
+    for (var i = 0; i <= 0; i++) {
+    start()
+}
+    function start() {
+        if (P) {
+            readline.question(`Do yo want to write a new program or to run the current program\n 1->run current program\n 2->write a new program\n`, value => {
+                if (value == `1`) {
+                    require(`./run`)
+                    fs.writeFileSync("./save.txt", ``)
+                    fs.writeFileSync("./loop_counter.txt", ``)
+                }
+                else if (value == `2`) {
+                    fs.writeFileSync("./program.txt", ``)
+                    fs.writeFileSync("./save.txt", ``)
+                    fs.writeFileSync("./loop_counter.txt", ``)
+                    code()
+                } else {
+                    process.exit()
+                }
+            })
+        } else {
+            code()
+        }
+    }
+    function code() {
+        readline.question(`${line})`, value => {
+            if (value.toLowerCase() !== `end`) {
+                fs.readFile("./program.txt", "utf8", (err, program) => {
+                    let Code = (!program) ? `` : program + `\n`
+                    fs.writeFileSync("./program.txt", Code + value)
+                })
+                line = line + 1
+                code()
+            } if (value.toLowerCase() == `end`) {
+                readline.close()
+                require(`./run`)
+            }
+        })
+    }
+})
+
---
+++ run.js
@@ -0,0 +1,181 @@
+const fs = require('fs');
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var answer = []
+var input_1 = []
+var input_2 = []
+
+fs.readFile("./program.txt", "utf8", (err, code) => {
+
+    var line = 0
+    console.log(`running program`)
+
+    function run() {
+
+        setInterval(() => {
+            fs.readFile("./save.txt", "utf8", (err, s) => {
+
+                let L = code.toString().replace(/ /g, ``).replace(/\n/g, `*`).replace(/[a-z]/g, ``).replace(/[A-Z]/g, ``).replace(/[0-9]/g, ``)
+                if (line >= L.length && L.length > 0 || !L && line == 2) {
+
+                    return false
+                } else if (L.length <= 0 && line < 1) {
+
+                    if (code.toLocaleLowerCase().startsWith(`say`)) {
+                        setTimeout(() => {
+                            const print = code.split(` `).slice(1).join(` `).toString().replace(/save/g, `${s}`).replace(/input1/g, `${input_1}`)
+                            console.log(print)
+
+                        }, 6)
+                    }
+                    if (code.toLocaleLowerCase().startsWith(`count`)) {
+                        setTimeout(() => {
+                            let number = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            number = (number < 0) ? (number * -1) + 1 : number + 1
+                            let n = 0
+
+                            function count() {
+                                if (n < number) {
+                                    console.log(n)
+                                    n = n + 1
+                                    count()
+                                } else if (n >= number) return;
+                            }
+                            count()
+                        }, 5)
+                    }
+                    line = line + 1
+
+                    return false
+
+                } else if (line < L.length + 1 && L) {
+
+                    const code_line = code.replace(/\n/g, `*`).split('*').slice(line, line + 1).toString()
+
+                    if (code_line.toLocaleLowerCase().startsWith(`say`)) {
+                        setTimeout(() => {
+                            const print = code_line.split(` `).slice(1).join(` `).toString().replace(/save/g, `${s}`).replace(/input1/g, `${input_1}`)
+                            if (print) {
+                                console.log(print)
+                            }
+                        }, 7)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`loop`)) {
+                        fs.readFile("./loop_counter.txt", "utf8", (err, nums) => {
+                            setTimeout(() => {
+                                let num_line = Number(code_line.split(` `).slice(1, 2).toString())
+                                let rounds = Number(code_line.split(` `).slice(2, 3).toString())
+                                rounds = (!rounds) ? Infinity : rounds
+                                if (rounds > Number(nums)) {
+                                    line = num_line - 1
+                                    fs.writeFileSync("./loop_counter.txt", `${Number(nums) + 1}`)
+                                } else if (rounds <= Number(nums)) {
+                                    return;
+                                }
+                            }, 5)
+                        })
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`count`)) {
+                        setTimeout(() => {
+                            let number = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            number = (number < 0) ? (number * -1) + 1 : number + 1
+                            let n = 0
+
+                            function count() {
+                                if (n + 1 < number + 1) {
+                                    if (n >= number + 1) return false
+                                    else {
+                                        console.log(n)
+                                        n = (n + 1 < number + 1) ? n + 1 : n
+                                        count()
+                                    }
+                                }
+                            }
+                            count()
+                        }, 6)
+                    }
+                    if (code_line.toLowerCase().startsWith(`saves`)) {
+                        setTimeout(() => {
+                            let slot = code_line.split(` `).slice(1, 2).toString().toLowerCase()
+                            let pre_line = line - 2
+                            const wanted_line = code.replace(/\n/g, `*`).split('*').slice(pre_line, pre_line + 1).toString()
+                            let wanted_value = (slot.includes(`t`)) ? wanted_line.split(` `).slice(1).join(` `).toString() : (slot.includes(`a`)) ? answer : answer
+                            fs.writeFileSync("./save.txt", `${wanted_value}`)
+                            console.log()
+                        }, 6)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`add`)) {
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code_line.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a + b
+                            answer = answer2
+                            console.log(`${a} + ${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`divide`)) {
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code_line.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a / b
+                            answer = answer2
+                            console.log(`${a}/${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`subtract`)) {
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code_line.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a - b
+                            answer = answer2
+                            console.log(`${a} - ${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`multiply`)) {
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code_line.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a * b
+                            answer = answer2
+                            console.log(`${a}*${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    if (code_line.toLocaleLowerCase().startsWith(`random`)) {
+                        function mode(n) {
+                            return Math.floor(Math.random() * n);
+                        }
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+
+                            const answer2 = mode(a) +1
+                            answer = answer2
+                            console.log(`${answer2}`)
+                        }, 7)
+                    }
+
+                    line = line + 1
+
+                }
+            })
+        }, 1)
+
+    }
+    setInterval(() => {
+        run()
+    }, 1000)
+})
+
+/*
+commands
+say -> prints what you type
+loop -> loop back to the code line 
+count -> counts from 0 to a number 
+saves a/t -> saves the last value that been used
+divide -> divide a by b (a/b)
+add -> add a to b (a+b)
+subtract -> subtract a from b(a - b)
+multiply - > multiply a and b (a*b)
+ */
