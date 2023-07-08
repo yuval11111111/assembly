89be2988b8e5f6f6b57c9f599d4e9566ab037997
yuval11111111 <87775280+yuval11111111@users.noreply.github.com>
2023-07-08 21:36:43 +0300

    Add files via upload

--- run.js
+++ run.js
@@ -1,29 +1,36 @@
+//imports
 const fs = require('fs');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//variables
 var answer = []
 var input_1 = []
 var input_2 = []
 
+//read the code
 fs.readFile("./program.txt", "utf8", (err, code) => {
 
+    //line counter
     var line = 0
     console.log(`running program`)
 
+    //main function
     function run() {
 
         setInterval(() => {
             fs.readFile("./save.txt", "utf8", (err, s) => {
 
+                //length calculator
                 let L = code.toString().replace(/ /g, ``).replace(/\n/g, `*`).replace(/[a-z]/g, ``).replace(/[A-Z]/g, ``).replace(/[0-9]/g, ``)
+                //one command program
                 if (line >= L.length && L.length > 0 || !L && line == 2) {
 
                     return false
                 } else if (L.length <= 1 && line < 1) {
-
+                    //say one command
                     if (code.toLocaleLowerCase().startsWith(`say`)) {
                         setTimeout(() => {
                             const print = code.split(` `).slice(1).join(` `).toString().replace(/save/g, `${s}`).replace(/input1/g, `${input_1}`)
@@ -31,6 +38,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
 
                         }, 6)
                     }
+                    //count one command
                     if (code.toLocaleLowerCase().startsWith(`count`)) {
                         let number = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`).toString())
                         setTimeout(() => {
@@ -54,6 +62,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             }
                         }, 7)
                     }
+                    //random one command
                     if (code.toLocaleLowerCase().startsWith(`random`)) {
                         function mode(n) {
                             return Math.floor(Math.random() * n);
@@ -66,15 +75,65 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${answer2}`)
                         }, 7)
                     }
-
+                    //power one command
+                    if (code.toLocaleLowerCase().startsWith(`power`)) {
+                        setTimeout(() => {
+                            let a = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            let answer2 = Math.pow(a, b)
+                            answer = answer2
+                            console.log(`${a}^${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    //add one command
+                    if (code.toLocaleLowerCase().startsWith(`add`)) {
+                        setTimeout(() => {
+                            let a = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a + b
+                            answer = answer2
+                            console.log(`${a} + ${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    //divide one command
+                    if (code.toLocaleLowerCase().startsWith(`divide`)) {
+                        setTimeout(() => {
+                            let a = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a / b
+                            answer = answer2
+                            console.log(`${a}/${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    //subtract one command
+                    if (code.toLocaleLowerCase().startsWith(`subtract`)) {
+                        setTimeout(() => {
+                            let a = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            const answer2 = a - b
+                            answer = answer2
+                            console.log(`${a} - ${b} = ${answer2}`)
+                        }, 7)
+                    }
+                    //multiply one command
+                    if (code.toLocaleLowerCase().startsWith(`multiply`)) {
+                        setTimeout(() => {
+                            let a = Number(code.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            let answer2 = a * b
+                            answer = answer2
+                            console.log(`${a}*${b} = ${answer2}`)
+                        }, 7)
+                    }
                     line = line + 1
 
                     return false
 
                 } else if (line < L.length + 1 && L) {
 
+                    //getting current line command
                     const code_line = code.replace(/\n/g, `*`).split('*').slice(line, line + 1).toString()
-
+                    //say multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`say`)) {
                         setTimeout(() => {
                             const print = code_line.split(` `).slice(1).join(` `).toString().replace(/save/g, `${s}`).replace(/input1/g, `${input_1}`)
@@ -83,6 +142,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             }
                         }, 7)
                     }
+                    //loop
                     if (code_line.toLocaleLowerCase().startsWith(`loop`)) {
                         fs.readFile("./loop_counter.txt", "utf8", (err, nums) => {
                             setTimeout(() => {
@@ -98,6 +158,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             }, 7)
                         })
                     }
+                    //count multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`count`)) {
                         let number = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`).toString())
                         setTimeout(() => {
@@ -122,6 +183,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             }
                         }, 7)
                     }
+                    //saves
                     if (code_line.toLowerCase().startsWith(`saves`)) {
                         setTimeout(() => {
                             let slot = code_line.split(` `).slice(1, 2).toString().toLowerCase()
@@ -130,8 +192,9 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             let wanted_value = (slot.includes(`t`)) ? wanted_line.split(` `).slice(1).join(` `).toString() : (slot.includes(`a`)) ? answer : answer
                             fs.writeFileSync("./save.txt", `${wanted_value}`)
                             console.log()
-                        }, 6)
+                        }, 5)
                     }
+                    //add multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`add`)) {
                         setTimeout(() => {
                             let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
@@ -141,6 +204,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${a} + ${b} = ${answer2}`)
                         }, 7)
                     }
+                    //divide multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`divide`)) {
                         setTimeout(() => {
                             let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
@@ -150,6 +214,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${a}/${b} = ${answer2}`)
                         }, 7)
                     }
+                    //subtract multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`subtract`)) {
                         setTimeout(() => {
                             let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
@@ -159,6 +224,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${a} - ${b} = ${answer2}`)
                         }, 7)
                     }
+                    //multiply multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`multiply`)) {
                         setTimeout(() => {
                             let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
@@ -168,6 +234,7 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${a}*${b} = ${answer2}`)
                         }, 7)
                     }
+                    //random multi-command
                     if (code_line.toLocaleLowerCase().startsWith(`random`)) {
                         function mode(n) {
                             return Math.floor(Math.random() * n);
@@ -180,9 +247,20 @@ fs.readFile("./program.txt", "utf8", (err, code) => {
                             console.log(`${answer2}`)
                         }, 7)
                     }
+                    //forget
                     if (code_line.toLocaleLowerCase() == `forget`) {
                         setTimeout(() => {
                             fs.writeFileSync("./save.txt", ``)
+                        }, 7)
+                    }
+                    //power multi-command
+                    if (code_line.toLocaleLowerCase().startsWith(`power`)) {
+                        setTimeout(() => {
+                            let a = Number(code_line.split(` `).slice(1, 2).toString().replace(/save/, `${s}`))
+                            let b = Number(code_line.split(` `).slice(2, 3).toString().replace(/save/, `${s}`))
+                            let answer2 = Math.pow(a, b)
+                            answer = answer2
+                            console.log(`${a}^${b} = ${answer2}`)
                         }, 7)
                     }
                     /*
@@ -219,5 +297,6 @@ subtract -> subtract a from b(a - b)
 multiply -> multiply a and b (a*b)
 random -> randomly pick a number between 1 - x 
 forget -> clear the memory from the last saved info in the run
+power -> do a to the power of b (a^b)
 *debug -> saves the values of specific lines into a txt file*
  */
